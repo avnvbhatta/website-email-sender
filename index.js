d286567a3250c12cb063d848c9aebf376e1a7529
abhatta <avnvbhatta@gmail.com>
2020-09-20 20:53:15 -0700

    Initial commit. Working email sender.

---
+++ index.js
@@ -0,0 +1,54 @@
+//Required dependencies
+const dotenv = require('dotenv').config();
+const nodemailer = require("nodemailer");
+const express = require('express');
+const bodyParser = require('body-parser')
+
+//Initialize Express
+const app = express();
+//Set port 
+const port = 3008;
+
+app.use(bodyParser.json()); 
+app.use( bodyParser.urlencoded({ extended : true }) );
+
+//POST route to receive user data and send email
+app.post('/', async (req, res) => {
+    //Get form data
+    const {senderName, senderEmail, message} = req.body;
+    try{
+        let mailAccount = await nodemailer.createTestAccount();
+
+        // create reusable transporter object using the default SMTP transport
+        let transporter = nodemailer.createTransport({
+            host: "smtp.gmail.com",
+            port: 465,
+            secure: true, // true for 465, false for other ports
+            auth: {
+            user: process.env.GMAIL_NODEMAILER_EMAIL, // gmail email
+            pass: process.env.GMAIL_NODEMAILER_PASSWORD, // gmail password
+            },
+        });
+    
+        // send mail with defined transport object
+        let info = await transporter.sendMail({
+            from: `${senderName} <${senderEmail}>`, // sender address
+            to: `${process.env.GMAIL_NODEMAILER_EMAIL}, ${process.env.GMAIL_PRIMARY}`, // list of receivers
+            subject: "Important! Email from website!", // Subject line
+            html: `<h1>Message from ${senderName}</h1><br>${message}`, // html body
+        });
+
+        res.status(200).send("Message successfully sent.")
+    }
+    catch(error){
+        console.log(error)
+        res.status(500).send("An error occured.");
+    }
+    
+
+})
+  
+ 
+app.listen(port, () => {
+    console.log(`Mailer app listening at port ${port}`)
+})
